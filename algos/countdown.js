309fb2639e442c2c2cb2eb57f1b2518570289ed2
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2022-06-13 09:05:41 -0700

    pb w2 algos

---
+++ algos/countdown.js
@@ -0,0 +1,49 @@
+console.log("Wazzaaaap");
+
+// console.log(5);
+// console.log(4);
+// console.log(3);
+// console.log(2);
+// console.log(1);
+
+// to comment something out, the hotkey is (control + ? on windows) (command + ? on mac)
+
+
+
+
+
+function countDown(startNum, endNum){
+    for(let count=startNum; count>=endNum; count--){
+        if(count == 23){
+            console.log("The king from Cleveland");
+        }else{
+            console.log(count);
+        }
+    }
+    console.log("finished the function");
+}
+
+countDown(30, 0);
+// countDown(3, -10);
+// countDown(1);
+
+
+
+
+
+
+// function orderPizza(firstName, lastName){ //we have two PARAMETERS (firstName and lastName)
+//     console.log("Ordering pizza for " + firstName + " " + lastName);
+// }
+
+// orderPizza("Lebron", "James"); //the inputs you give to the function when you call the function are called ARGUMENTS
+// orderPizza("Brandon", "Mendoza"); 
+// orderPizza("Derek", "Haig"); 
+
+
+
+console.log("finished!")
+
+//1. create a variable called count and start (initialize) it at 5
+//2. continue doing the for loop as long as count is greater than 0. When count is no longer greater than 0, stop the loop
+//3. in each iteration, decrement (decrease) the count by 1
