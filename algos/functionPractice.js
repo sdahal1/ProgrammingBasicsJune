b7f9f557fee6cf2906ce0caff08d4fd94893df38
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2022-06-17 12:11:25 -0700

    function prac

---
+++ algos/functionPractice.js
@@ -0,0 +1,9 @@
+function sayHello(firstName, lastName){ //parameters are what the function will recieve
+    console.log("Hello " + firstName + " " + lastName);
+}
+
+
+
+sayHello("Andy", "Lim"); //arguements (andy lim) are what the function will be given
+sayHello("Rob", "Dahal");
+
